f4af54057b582c719907717292dacc3444aedc43
Felipe Munevar N <felipemunevarn@gmail.com>
2024-02-25 14:33:51 -0300

    add condition in case carro is empty

--- viasus/carro/context_processor.py
+++ viasus/carro/context_processor.py
@@ -1,6 +1,7 @@
 def total(request):
     total = 0
     if (request.user.is_authenticated):
-        for key, value in request.session["carro"].items():
-            total += float(value["precio"]) * value["cantidad"]
+        if (request.session["carro"] != {}):
+            for key, value in request.session["carro"].items():
+                total += float(value["precio"]) * value["cantidad"]
     return {"total": total}
