52b7cf7d1add01431f047dbb5e9ae50157218bd8
Felipe Munevar N <felipemunevarn@gmail.com>
2024-04-08 23:58:34 -0300

    change settings to find catalogo

--- viasus/viasus/settings.py
+++ viasus/viasus/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalogo.apps.CatalogoConfig',
+    'viasus.catalogo.apps.CatalogoConfig',
     'carro.apps.CarroConfig',
     'authentication.apps.AuthenticationConfig',
     'viasus',
