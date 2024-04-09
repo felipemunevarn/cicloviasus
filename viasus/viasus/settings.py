11733ef82b84729d82e20117950189f84ba87623
Felipe Munevar N <felipemunevarn@gmail.com>
2024-04-09 00:26:57 -0300

    change settings to add __init__ in viasus

--- viasus/viasus/settings.py
+++ viasus/viasus/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalogo',
+    'viasus.catalogo',
     'carro.apps.CarroConfig',
     'authentication.apps.AuthenticationConfig',
     'viasus',
