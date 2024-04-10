291dd2fffbc65312035dc69e62144d9722953f3f
Felipe Munevar N <felipemunevarn@gmail.com>
2024-04-10 12:20:47 -0300

    change static path and upper search field

--- viasus/catalogo/views.py
+++ viasus/catalogo/views.py
@@ -7,7 +7,7 @@ def catalogue(request):
     if (request.GET.get('busqueda') == ''):
         result = Producto.objects.all().order_by("titulo")
     else:
-        result = Producto.objects.filter(titulo__contains=request.GET.get('busqueda', '')).order_by("titulo")
+        result = Producto.objects.filter(titulo__contains=request.GET.get('busqueda', '').upper()).order_by("titulo")    
     for product in result:
         product.precio = int(product.precio)
     return render(request, "catalogue.html", {"result": result})
--- viasus/viasus/settings.py
+++ viasus/viasus/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -58,12 +57,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CRON_CLASSES = [
-#     "viasus.MyCronJob.MyCronJob",
-# ]
+CRON_CLASSES = [
+    "viasus.MyCronJob.MyCronJob",
+]
 
-ROOT_URLCONF = 'viasus.urls'
-# ROOT_URLCONF = 'urls'
+# ROOT_URLCONF = 'viasus.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -138,7 +137,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = 'C:/Users/Administrator/Documents/cicloviasus/viasus/static'
+# STATIC_ROOT = 'C:/Users/Administrator/Documents/cicloviasus/viasus/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -151,3 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
