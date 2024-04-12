d68d39fd7a1006908893042d0c6f28cccda98b00
Felipe Munevar N <felipemunevarn@gmail.com>
2024-04-11 23:33:08 -0300

    add BASE_DIR to STATIC_URL

--- viasus/viasus/settings.py
+++ viasus/viasus/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
