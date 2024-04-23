265029a1961d3ac95c60ff0bfde7920145e6e369
Felipe Munevar N <felipemunevarn@gmail.com>
2024-04-22 23:46:49 -0300

    generate report of today checkouts

--- viasus/viasus/views.py
+++ viasus/viasus/views.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 from django.shortcuts import render
+from carro.create_excel import create_excel
+from carro.models import Pedido, PedidoProducto
 
 # Create your views here.
 
@@ -9,4 +12,17 @@ def about(request):
     return render(request, "about.html")
 
 def resume(request):
+    today = datetime.now().date().strftime("%Y-%m-%d")
+    day_checkouts = Pedido.objects.filter(fecha_pedido__contains=today)
+    carro = {}
+    for checkout in day_checkouts:
+        asked_products = PedidoProducto.objects.filter(pedido_id=checkout.id).select_related()
+        for product in asked_products:
+            if (str(product.producto.id) in carro):
+                carro.get(str(product.producto.id)).update({ 'cantidad':carro.get(str(product.producto.id))["cantidad"] + product.cantidad})
+            else:
+                carro.update({str(product.producto.id): {}})
+                carro.get(str(product.producto.id)).update({ 'cantidad': product.cantidad })
+    create_excel(request="", daily_cart=carro)
+    # send_mail_excel("", "", "", True, today)
     return render(request, "resume.html")
